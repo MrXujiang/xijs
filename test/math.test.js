a19121dad266610e05162b142da008f8d7158865
EasyRo <easyro1998@163.com>
2023-04-13 22:57:54 +0800

    fix shuffle test

--- test/math.test.js
+++ test/math.test.js
@@ -34,9 +34,17 @@ describe('数学计算相关测试', () => {
   });
 
   test('打乱数组', () => {
-    const arr = [1, 3, 5, 7, 4, 3];
-    const shuffleArr = shuffle(arr.slice());
-    expect(shuffleArr).not.toEqual(arr);
+    const arr = [1, 3, 5, 7, 4, 3],
+      arrStr = arr.toString();
+    let shuffleCount = 0;
+    for (let i = 0; i < 100; i++) {
+      const shuffleArr = shuffle(arr.slice());
+      if (shuffleArr.toString() !== arrStr) {
+        shuffleCount++;
+      }
+    }
+    const probability = shuffleCount / 100;
+    expect(probability).toBeGreaterThanOrEqual(0.9);
   });
 
   test('求和', () => {
