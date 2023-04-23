49218be56776936b39427eaf50ba45e10c657de8
Kevin <18392136187@163.com>
2023-04-23 15:55:06 +0800

    使用 type 字符串面量，优化switch

--- src/dateCalculate/index.ts
+++ src/dateCalculate/index.ts
@@ -1,5 +1,31 @@
 import {formatDate} from "../index";
 
+type DateType = 'year' | 'month' | 'day' | 'hour' | 'minute' | 'second' | 'milliseconds';
+
+const dateTypeMap = {
+  year: (date: Date, num: number) => {
+    return date.setFullYear(date.getFullYear() + num)
+  },
+  month: (date: Date, num: number) => {
+    return date.setMonth(date.getMonth() + num);
+  },
+  day: (date: Date, num: number) => {
+    return date.setDate(date.getDate() + num);
+  },
+  hour: (date: Date, num: number) => {
+    return date.setHours(date.getHours() + num);
+  },
+  minute: (date: Date, num: number) => {
+    return date.setMinutes(date.getMinutes() + num);
+  },
+  second: (date: Date, num: number) => {
+    return date.setSeconds(date.getSeconds() + num);
+  },
+  milliseconds: (date: Date, num: number) => {
+    return date.setMilliseconds(date.getMilliseconds() + num);
+  },
+}
+
 /**
  * 对日期进行计算，比如计算前一天，前一个月，后一天，后一个月等等
  * @param dateStr: 需要计算的日期字符串或者 Date
@@ -7,34 +33,14 @@ import {formatDate} from "../index";
  * @param flag：对 年月日时分秒 那个进行计算
  * @param format：对 计算后的日期进行格式化的格式，默认使用 YY-MM-DD hh:mm:ss。比如：2020-01-02 08:00:00
  */
-const dateCalculate = (dateStr: string | Date, num: number, flag: string, format?: string): string => {
-  let date = new Date(dateStr)
-  switch (flag) {
-    case 'year':
-      date.setFullYear(date.getFullYear() + num);
-      break
-    case 'month':
-      date.setMonth(date.getMonth() + num);
-      break
-    case 'day':
-      date.setDate(date.getDate() + num);
-      break
-    case 'hour':
-      date.setHours(date.getHours() + num);
-      break
-    case 'minute':
-      date.setMinutes(date.getMinutes() + num);
-      break
-    case 'second':
-      date.setSeconds(date.getSeconds() + num);
-      break
-    case 'milliseconds':
-      date.setMilliseconds(date.getMilliseconds() + num);
-      break
-    default:
-      throw new Error("时间计算有误，不支持 年月日时分秒毫秒 以外的计算")
+const dateCalculate = (dateStr: string | Date, num: number, flag: DateType, format?: string): string => {
+  if (dateTypeMap[flag]) {
+    let date = new Date(dateStr)
+    let result = dateTypeMap[flag](date, num);
+    return formatDate(result, format)
+  } else {
+    throw Error("请输入正确的正则类型");
   }
-  return formatDate(date.getTime(), format)
 }
 
 export default dateCalculate;
