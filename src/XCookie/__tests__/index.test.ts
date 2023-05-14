53217c4c5e45d9d71ec5f930f8ec760cb876fe1b
EasyRo <easyro1998@163.com>
2023-05-14 14:31:38 +0800

    docs: rename xCookie

--- src/XCookie/__tests__/index.test.ts
+++ src/XCookie/__tests__/index.test.ts
@@ -1,26 +1,26 @@
-import XCookie from '../index';
+import xCookie from '../index';
 import sleep from '../../sleep';
 
 describe('XCookie', () => {
   test('增加，删除，获取所有 cookie 的操作', async () => {
     const key = 'foo',
       value = 'bar';
-    XCookie.set(key, value, { maxAge: 3 });
-    expect(XCookie.get(key)).toBe(value);
+    xCookie.set(key, value, { maxAge: 3 });
+    expect(xCookie.get(key)).toBe(value);
     await sleep(3000);
-    expect(XCookie.get(key)).toBe('');
+    expect(xCookie.get(key)).toBe('');
 
-    const cookies = {
+    const cookies: { [propName: string]: string } = {
       foo: 'bar',
       foo1: 'bar1',
       foo2: 'bar2',
     };
     for (const key in cookies) {
-      XCookie.set(key, cookies[key]);
+      xCookie.set(key, cookies[key]);
     }
-    XCookie.remove('foo');
+    xCookie.remove('foo');
     Reflect.deleteProperty(cookies, 'foo');
 
-    expect(XCookie.allCookies()).toEqual(cookies);
+    expect(xCookie.allCookies()).toEqual(cookies);
   });
 });
